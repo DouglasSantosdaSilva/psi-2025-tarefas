ce8e720bbd723b207dcc46f1e72b8b9c1482c7af
douglas <douglassantos99249@gmail.com>
2025-10-17 15:38:21 -0300

    finalizando a tarefa05

--- tarefa05/config/settings.py
+++ tarefa05/config/settings.py
@@ -59,6 +59,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
